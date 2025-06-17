db95ccf3a8dea753af6d94930f6c7ef0a3b6af3b
Niraj1412 <14nirajkumar@gmail.com>
2025-06-18 04:46:29 +0530

    brushing 4

--- src/pages/TranscriptGridPage.js
+++ src/pages/TranscriptGridPage.js
@@ -146,12 +146,7 @@ const fetchVideoDetails = async (videoId) => {
     }
 
     // Parse duration from ISO format or seconds
-    let durationInSeconds;
-    if (details.duration && typeof details.duration === 'string') {
-      durationInSeconds = parseISODuration(details.duration);
-    } else {
-      durationInSeconds = Number(details.duration) || 0;
-    }
+   let durationInSeconds = Number(details.duration) || 0;
 
     // Determine thumbnail URL
     let thumbnailUrl;
