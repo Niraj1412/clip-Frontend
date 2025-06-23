6dacc1fae344eb49ba35a1c9562839d430ba37ac
Niraj1412 <14nirajkumar@gmail.com>
2025-06-23 14:31:09 +0530

    brushing 31

--- src/components/TwitterCallback.js
+++ src/components/TwitterCallback.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import authService from '../../services/authService';
+import authService from '../services/authService';
 
 const TwitterCallback = () => {
   const navigate = useNavigate();
